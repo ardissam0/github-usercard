5a0011971e08a4a0fe642f1092a5f782b223796e
Sam Ardis <ardis.productions@gmail.com>
2020-09-16 14:35:52 -0400

    Styling changes, made mobile friendly

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,6 +1,3 @@
-/* Step 1: using axios, send a GET request to your github
-           https://api.github.com/users/<your name>
-*/
 
 const cards = document.querySelector('.cards');
 
@@ -14,28 +11,6 @@ axios.get('https://api.github.com/users/ardissam0')
   console.log(err);
 });
 
-/* Step 2: Inspect and study the data coming back. Tou will need to understand the structure of this 
-   data in order to use it to build your component function 
-   Skip to Step 3.
-*/
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
-
-<div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
-    <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:  
-      <a href={address to users github page}>{address to users github page}</a>
-    </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
-</div>
-*/
 function createComponent(gitUser) {
   const card = document.createElement('div');
   const img = document.createElement('img');
@@ -69,21 +44,6 @@ function createComponent(gitUser) {
 return card;
 };
 
-/* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
-*/
-
-
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
-
 const followersArray = [];
 
 axios.get('https://api.github.com/users/ardissam0/followers')
@@ -101,13 +61,3 @@ axios.get('https://api.github.com/users/ardissam0/followers')
   });
 
   console.log(followersArray);
-
- 
-
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
